205ba9fa444c686476a0e01d431a4ab68c07698d
Rodrigo Méro <meroo.rodrigo@gmail.com>
2022-02-02 19:30:00 +0000

    torre esta funcional

--- src/torre.js
+++ src/torre.js
@@ -1,32 +1,61 @@
 /* VARIAVEIS GLOBAIS */
 const barras = document.querySelectorAll(".torre")
 const container = document.getElementById("container-geral")
+const resetButton = document.getElementById("reset-button")
+const comemoracao = document.querySelector("h2")
 let hold = null
+let counter = document.getElementById("counter")
+let count = 0
+let dificuldade 
 
-//corrigir quando clicar numa barra vazia, nao fazer nada
-for(let i = 0;i<barras.length;i++){
-    barras[i].addEventListener("click", function(event){
-        let alvo = event.currentTarget
-        console.log(alvo)
-        if(hold == null){
-            hold = alvo.lastElementChild
-        }else{
-            alvo.appendChild(hold)
-            hold = null
-        }    
-    })
+resetButton.addEventListener("click", resetGame)
+
+container.addEventListener("click", function(event){
+    let alvo = event.target.closest(".torre")
+    
+    if(hold == null && alvo.childElementCount == 0){
+        
+    }else if(hold == null){
+        hold = alvo.lastElementChild
+    }else if(alvo.childElementCount == 0 || hold.clientWidth < alvo.lastElementChild.clientWidth){
+        alvo.appendChild(hold)
+        hold = null
+        count++
+        counter.innerText = "Movimentos: " + count
+    }
+    if(hold.clientWidth >= barras[0].lastElementChild.clientWidth && hold.clientWidth >= barras[1].lastElementChild.clientWidth && hold.clientWidth >= barras[2].lastElementChild.clientWidth){
+        console.log("travou")
+        hold = null
+    }
+    checkWin(barras[2])
+})
+
+function checkWin(ultimaBarra){
+    if(ultimaBarra.childElementCount == dificuldade){
+        comemoracao.classList.remove("hidden")
+    }
 }
 
-function ResetGame(){
-    barraEsq.innerHTML = ""
-    barraCentro.innerHTML = ""
-    barraDir.innerHTML = ""
+function resetGame(){
+    const seletores = document.querySelectorAll("input")
+    seletores.forEach(function (seletor){
+        if(seletor.checked){
+            dificuldade = seletor.value
+        }
+    })
 
-    const vermelho = document.createElement("div")
-    vermelho.classList.add("red","disco")
+    barras.forEach(barra => barra.innerHTML = "")
 
-    const verde = document.createElement("div")
-    verde.classList.add("green", "disco")
+    if( dificuldade > 4 ){
+        const vermelho = document.createElement("div")
+        vermelho.classList.add("red","disco")
+        barras[0].appendChild(vermelho)
+    }
+    if( dificuldade > 3){
+        const verde = document.createElement("div")
+        verde.classList.add("green", "disco")
+        barras[0].appendChild(verde)
+    }
 
     const roxo = document.createElement("div")
     roxo.classList.add("purple", "disco")
@@ -34,9 +63,17 @@ function ResetGame(){
     const azul = document.createElement("div")
     azul.classList.add("blue", "disco")
 
-    barraEsq.appendChild(vermelho)
-    barraEsq.appendChild(verde)
-    barraEsq.appendChild(roxo)
-    barraEsq.appendChild(azul)
+    const amarelo = document.createElement("div")
+    amarelo.classList.add("yellow", "disco")
 
+    barras[0].appendChild(roxo)
+    barras[0].appendChild(azul)
+    barras[0].appendChild(amarelo)
+
+    comemoracao.classList.add("hidden")
+    
 }
+
+resetGame()
+
+
