4dae0496807a20905bf0e3a0c8294d53c9bc7762
Rodrigo Méro <meroo.rodrigo@gmail.com>
2022-02-02 00:29:44 +0000

    Innitial Work, missing the extras

---
+++ src/torre.js
@@ -0,0 +1,42 @@
+/* VARIAVEIS GLOBAIS
+const barras = document.querySelectorAll(".torre")
+const container = document.getElementById("container-geral")
+let hold = null
+*/
+
+for(let i = 0;i<barras.length;i++){
+    barras[i].addEventListener("click", function(event){
+        let alvo = event.currentTarget
+        console.log(alvo)
+        if(hold == null){
+            hold = alvo.lastElementChild
+        }else{
+            alvo.appendChild(hold)
+            hold = null
+        }    
+    })
+}
+
+function ResetGame(){
+    barraEsq.innerHTML = ""
+    barraCentro.innerHTML = ""
+    barraDir.innerHTML = ""
+
+    const vermelho = document.createElement("div")
+    vermelho.classList.add("red","disco")
+
+    const verde = document.createElement("div")
+    verde.classList.add("green", "disco")
+
+    const roxo = document.createElement("div")
+    roxo.classList.add("purple", "disco")
+
+    const azul = document.createElement("div")
+    azul.classList.add("blue", "disco")
+
+    barraEsq.appendChild(vermelho)
+    barraEsq.appendChild(verde)
+    barraEsq.appendChild(roxo)
+    barraEsq.appendChild(azul)
+
+}
